f670c962c465701f8ed03cb7ad89ebb0f0bf3555
Mike Butler <michael.butler@progress.com>
2022-10-14 13:29:02 -0400

    1.3.1 remove unless data_collector

--- libraries/capture-converge-status.rb
+++ libraries/capture-converge-status.rb
@@ -4,8 +4,7 @@ class Chef
       private
 
       def send_to_data_collector(message)
-        return unless data_collector_accessible?
-        ::FILE.write('/tmp/converge-status.json', JSON.generate(message))
+        IO.write('/tmp/converge-status.json', JSON.generate(message))
         http.post(nil, message, headers)
       end
     end
