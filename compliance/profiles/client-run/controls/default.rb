99872317c113f9f827686534fd53ef8b59f0be91
Mike Butler <michael.butler@progress.com>
2023-12-01 08:10:37 -0500

    1.7.1 Removed MacOS Failures

--- compliance/profiles/client-run/controls/default.rb
+++ compliance/profiles/client-run/controls/default.rb
@@ -14,14 +14,15 @@ control 'chef-client' do
       its('version') { should cmp >= '17' }
     end
   elsif os.darwin?
-    describe launchd_service('chef.restarter') do
-      it { should be_installed }
-      it { should be_enabled }
-    end
-    describe launchd_service('chef.chef-client') do
-      it { should be_installed }
-      it { should be_enabled }
-    end
+# THESE ARE BROKEN WHEN RUNNING INSPEC AGAINST THE SERVER OUTSIDE OF THE CLIENT RUN
+#    describe launchd_service('chef.restarter') do
+#      it { should be_installed }
+#      it { should be_enabled }
+#    end
+#    describe launchd_service('chef.chef-client') do
+#      it { should be_installed }
+#      it { should be_enabled }
+#    end
     describe file('/Library/LaunchDaemons/com.chef.chef-client.plist') do
       its('content') { should include('<integer>300</integer>') }
     end
